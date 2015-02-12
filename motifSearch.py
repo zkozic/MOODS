ddd3e1084c6a1e7805d7738775f288330ee67c8b
Zrinko Kozic <zrinko.kozic@ed.ac.uk>
2015-02-12 14:13:50 +0000

    Implemented MOODS.search() function with treshold 0.001 and default values. Output is to stdout in format: <Sequence name>: Motif 1: hits no ...
    Doesn't really work good, lots of hits both in sequences and random controls.

--- motifSearch.py
+++ motifSearch.py
@@ -1,3 +1,35 @@
+##########
+# Usage:
+# python motifSearch.py <sequences.fa> <motifs.txt>
+##########
 import Bio
 from Bio import SeqIO
 import sys
+import MOODS._cmodule
+import math
+
+fasta = SeqIO.parse(sys.argv[1], 'fasta')
+sequences = {}
+for sequence in fasta:
+	sequences[str(sequence.id)] = str(sequence.seq)
+
+motifs = {}
+motifsfile = open(sys.argv[2], 'r')
+for line in motifsfile:
+	if line[0] == '>':
+		motif_name = line[1:-1]
+		motifs[motif_name] = []
+	else:
+		base = [int(i) for i in line.split()]
+		motifs[motif_name].append(base)
+
+motif_list = [motifs[i] for i in motifs]
+
+###Running MOODS
+for sequence in sequences:
+	print sequence
+	for motif in motifs:
+		hits = MOODS.search(sequences[sequence], [motifs[motif]], 0.001)
+		print 'Motif ' + motif + ': ' + str(len(hits[0]))
+	print ''
+#END
